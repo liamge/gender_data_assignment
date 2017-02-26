c3085eed4213497516236d524c3aa115745c12e0
Liam Geron <liams.geron@gmail.com>
2017-02-26 14:24:51 -0500

    Fix argv and opts

--- sarcasm_classifier.py
+++ sarcasm_classifier.py
@@ -13,28 +13,28 @@ conffile = sys.argv[1]
 
 ndata = -1 # for testing feature extraction: optional arg to control how much of data to use. won't work for testing classification because it just takes the first n -- all one class
 if len(argv) > 2:
-	ndata = argv[2]
+    ndata = argv[2]
 
 def load_data():
-	with open(datafile) as f:
-		return list(csv.reader(f))[0:ndata]
+    with open(datafile) as f:
+        return list(csv.reader(f))[0:ndata]
 
 def load_conf_file():
-	conf = set(line.strip() for line in open(conffile))
-	return conf
+    conf = set(line.strip() for line in open(conffile))
+    return conf
 
 def predict_sarcasm(X, Y):
-	scores = cross_val_score(svm.SVC(), X, Y, scoring='accuracy', cv=10)
-	return scores.mean()
+    scores = cross_val_score(svm.SVC(), X, Y, scoring='accuracy', cv=10)
+    return scores.mean()
 
 if __name__ == "__main__":
-	data = load_data()
-	conf = load_conf_file()
-	if 'f' in opts: 
-		with open(args[opts.index('f')]) as f:
-			features = list(csv.reader(f))
-	else:
-		features = fe.extract_features([line[-1] for line in data if line[0]=="GEN"], conf)
-	labels = [line[1] for line in data if line[0]=="GEN"]
+    data = load_data()
+    conf = load_conf_file()
+    if 'f' in opts:
+        with open(args[opts.index('f')]) as f:
+            features = list(csv.reader(f))
 
-	print (predict_sarcasm(features, labels))
+    features = fe.extract_features([line[-1] for line in data if line[0]=="GEN"], conf)
+    labels = [line[1] for line in data if line[0]=="GEN"]
+
+    print (predict_sarcasm(features, labels))
